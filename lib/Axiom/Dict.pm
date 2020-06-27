2fc76af4a006450cd72bbb03473e232629f8b678
Hugo van der Sanden <hv@crypt.org>
2020-06-27 13:44:28 +0100

    Document handling of names

--- lib/Axiom/Dict.pm
+++ lib/Axiom/Dict.pm
@@ -5,6 +5,59 @@ use warnings;
 
 use Axiom::Bind;
 
+=head1 NAME
+
+Axiom::Dict - bindings for function and variable names
+
+=head1 DESCRIPTION
+
+Objects of this class mediate access to names: functions and variables.
+
+Functions are handled as opaque operators. To overcome the ambiguity of
+C< x(y+1) > vs. C< f(y+1) >, function names must be declared in advance
+via the C< *func > directive. Declaration adds them to the current
+dictionary as the only available meaning for their declared name, and
+they link to an L<Axiom::Bind::Func> object.
+
+Variables are of two primary types: they may be external to an expression
+(ie a free variable) or internal to it (a bound variable). These are
+currently linked to L<Axiom::Bind::Var> and L<Axiom::Bind::Local> objects
+respectively, and are not opaque: various derivations involve specification
+or constraints relating to variables of appropriate types.
+
+All such variables, and the domain and codomain of functions, should be
+restricted to a particular well-defined set that can also be used as
+part of the reasoning. This is not yet implemented.
+
+External variables retain their meaning ("some specific (but unspecified)
+value x") across multiple expressions, from the point of declaration to
+the end of that scope. They may be declared explicitly with the C< *var >
+directive, or implicitly (eg in a C<condstart> derivation). To achieve
+this, we should introduce a new L<Axiom::Dict> instance for a new scope;
+that may either start empty (eg for a new file scope, as introduced with
+a C<*import> directive), or duplicating the current definitions of the
+outer scope for a conditional proof scope (C<condstart> to C<condend>)
+with C<< $dict->clone >> or C<< $dict->copy >>. FIXME: work out what
+the difference is between these, and remove one of them.
+
+Internal variables retain their meaning only within the subexpression
+to which they relate. For example C<sum(v, from, to, expr)> introduces
+the variable C<v> into the dictionary only when parsing the subexpression
+C<expr>. Their names are mutable, so it should be valid to substitute
+an expression such as a sum over C<i> into a part of the expression for
+another sum also declared over C<i> - if we need to serialize the result,
+the embedded variable should be rendered as something distinct (eg C<j>)
+to avoid ambiguity or error.
+
+For derivations we frequently need to parse isolated fragments of
+expressions, then relate them to a fragment of one or more well-formed
+expressions. To achieve this, we parse all expressions without attempting
+to look up the identity of names (other than function names), then put
+them through a separate resolution process L<Axiom::Expr/resolve> to
+relate each mention of a variable to its unique identity.
+
+=cut
+
 my $typeclass;
 BEGIN { $typeclass = Axiom::Bind->typeclass }
 
