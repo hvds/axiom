bc3afed05be6029984529119d535f1ffc56100e8
Hugo van der Sanden <hv@crypt.org>
2020-07-24 16:23:23 +0100

    Derive mapping for specify

--- lib/Axiom/Derive/Specify.pm
+++ lib/Axiom/Derive/Specify.pm
@@ -28,11 +28,55 @@ sub rulere { <<'RE' }
         ) })
 RE
 
-*derivere = \&rulere;
+sub derivere { <<'RE' }
+    <rule: specify>
+        specify (?: \( <[args=line]>? \) )?
+        (?{
+            $MATCH{args}[0] = $MATCH{args}[0]{args} if $MATCH{args};
+            $MATCH{args} //= [ '' ];
+        })
+RE
 
 sub derive {
     my($self, $args) = @_;
-    return $args;
+    my($line) = @$args;
+    my $starting = $self->line($line);
+    my $target = $self->expr;
+    $target->resolve($self->dict);
+
+    my %var;
+    my $seq = $starting;
+    while ($seq->is_quant) {
+        next unless $seq->type eq 'forall';
+        (my($var), $seq) = @{ $seq->args };
+        $var{$var->name} = $var;
+    }
+    my $teq = $target;
+    while ($teq->is_quant) {
+        next unless $teq->type eq 'forall';
+        (my($var), $teq) = @{ $teq->args };
+        delete $var{$var->name};
+    }
+
+    my @var = @var{ sort keys %var };
+    # FIXME: why does specify only allow a single var to be fixed?
+    die "Too many vars for specify" if @var > 1;
+
+    my $mapping = $self->find_mapping($seq, $teq, \@var);
+    # if we didn't find it, maybe one side has been simplified to the point
+    # we no longer recognise it
+    if (!$mapping && !$seq->is_atom && $seq->type eq $teq->type) {
+        my $sa = $seq->args;
+        my $ta = $teq->args;
+        if (@$sa == @$ta) {
+            for (0 .. $#$sa) {
+                $mapping = $self->find_mapping($sa->[$_], $ta->[$_], \@var);
+                last if $mapping;
+            }
+        }
+    }
+    return [ $line, $var[0], $mapping->{ $var[0]->name } ] if $mapping;
+    die "don't know how to derive this specify";
 }
 
 sub validate {
