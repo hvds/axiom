df0b829af0c34b30f37702341159c3bc274369dd
Hugo van der Sanden <hv@crypt.org>
2020-07-24 19:04:10 +0100

    Derive varmap for equate

--- lib/Axiom/Derive/Equate.pm
+++ lib/Axiom/Derive/Equate.pm
@@ -29,54 +29,51 @@ RE
 
 sub derivere { <<'RE' }
     <rule: equate>
-        equate \( <[args=optline]> <[args=line]> , <[args=varmap]> \)
+        equate \( <[args=optline]> <[args=line]> \)
         (?{ $MATCH{args}[$_] = $MATCH{args}[$_]{args} for (0, 1) })
 RE
 
 sub derive {
     my($self, $args) = @_;
-    my($line, $eqline, $map) = @$args;
+    my($line, $eqline) = @$args;
     my $starting = $self->line($line);
+    my $target = $self->expr;
+    $target->resolve($self->dict);
 
     my $from = $self->line($eqline);
     my $from_expr = $from;
-    my $from_loc = [];
+    my @vars;
     while ($from_expr->type eq 'forall') {
-        push @$from_loc, 2;
-        $from_expr = $from_expr->args->[1];
+        (my($var), $from_expr) = @{ $from_expr->args };
+        push @vars, $var;
     }
     $from_expr->type eq 'equals' or die sprintf(
         "Can't equate() with a %s\n", $from_expr->type,
     );
-    my $from_dict = $from->dict_at($from_loc);
-    my @from_var = map {
-        my($var, $expr) = @{ $_->{args} };
-        $var->resolve($from_dict);
-        $var->binding->id;
-    } @{ $map->{args} // [] };
 
-    my $target = $self->expr;
-    $target->resolve($self->dict);
-    my(@loc) = $starting->diff($target);
-    while (@loc) {
-        my $loc = shift @loc;
-        my $dict = $starting->dict_at($loc);
-        my $se = $starting->locate($loc);
-        my %vmap = map {
-            my($var, $expr) = @{ $map->{args}->[$_]->{args} };
-            $expr->resolve($dict);
-            ($from_var[$_] => $expr);
-        } (0 .. $#{ $map->{args} // [] });
-        my $equate = $from_expr->subst_vars(\%vmap);
-        my($left, $right) = @{ $equate->args };
-        if (! $se->diff($left)) {
-            return [ $line, $loc, $eqline, $map ];
-        } elsif (! $se->diff($right)) {
-            return [ $line, $loc, $eqline, $map ];
+    my @vargs;
+    my $try = sub {
+        my($loc, $map) = @_;
+        @vargs = ($line, $loc, $eqline, { args => [
+            map +{ args => [ $_->copy, $map->{$_->name} ] }, @vars,
+        ] });
+        local $self->{rules} = [];
+        local $self->{working} = $self->working;
+        return 0 unless eval { validate($self, \@vargs) };
+        return $self->working->diff($target) ? 0 : 1;
+    };
+
+    my $final;
+    $starting->walk_locn(sub {
+        return if $final;
+        my($se, $loc) = @_;
+        for my $from (@{ $from_expr->args }) {
+            my $map = $self->find_mapping($from, $se, \@vars)
+                    or next;
+            $final = \@vargs if $try->($loc, $map);
         }
-        next if $se->is_atom;
-        push @loc, map [ @$loc, $_ + 1 ], 0 .. $#{ $se->args };
-    }
+    });
+    return $final if $final;
     die "don't know how to derive this equate";
 }
 
